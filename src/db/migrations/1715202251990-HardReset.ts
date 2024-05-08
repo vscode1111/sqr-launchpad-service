963577406d80d4c4cc493ea7ed8ecd1282d6a246
Konstantin <scub111g@gmail.com>
2024-05-09 00:52:31 +0300

    fix: ci 2

--- src/db/migrations/1715202251990-HardReset.ts
+++ src/db/migrations/1715202251990-HardReset.ts
@@ -1,9 +1,13 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import { dbHardReset } from '../utils';
 
+const EXECUTE = true;
+
 export class HardReset1715202251990 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await dbHardReset(queryRunner);
+    if (EXECUTE) {
+      await dbHardReset(queryRunner);
+    }
   }
 
   public async down(): Promise<void> {}
