2472b0aa98c833e9783a3cc29961f8f59b9c5cb0
Andrei Shpak <andrei.s@msq.ai>
2024-07-09 07:39:19 +0000

    [MSQM-5777] migration rename

--- src/db/migrations/1720445136234-AlterContractType.ts
+++ src/db/migrations/1720445136234-AlterContractType.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AlterContractType1718039684392 implements MigrationInterface {
+export class AlterContractType1720445136234 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TYPE _contracts_type_enum ADD VALUE 'babt'`);
   }
