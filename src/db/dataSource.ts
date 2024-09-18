6693164161a688b3166b4317d4d31277f1ca9bdc
Konstantin <scub111g@gmail.com>
2024-09-18 16:08:38 +0300

    fix: ci 4

--- src/db/dataSource.ts
+++ src/db/dataSource.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions.js';
+//Do not change to '~common-service', otherwise "npm run db:migration" doesn't work
 import { dataSourceConfigBase, printDbConfig } from '~common-service/db';
 
 export const dataSourceConfig: PostgresConnectionOptions = {
